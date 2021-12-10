eafd719382271c1c9a49b2ead2ed88b12fd1e443
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-12-10 14:59:01 +0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -180,8 +180,8 @@
 		<Col xs="auto" style="display: flex;margin-bottom: 5px">
 			{#await getallusername() then value}
 				{#each value as item}
-					<!--Button color="primary" on:click={() => wowplayer(item.userid)}>{item.user}</Button-->
-					<Avatar randomBgColor initials="{item.user}" src="https://cravatar.eu/avatar/{item.user}" on:click={() => wowplayer(item.userid)}/>
+					<Button color="primary" on:click={() => wowplayer(item.userid)}><Avatar randomBgColor initials="{item.user}" src="https://cravatar.eu/avatar/{item.user}"/></Button>
+					<!--Avatar randomBgColor initials="{item.user}" src="https://cravatar.eu/avatar/{item.user}"/-->
 				{/each}
 			{/await}
 		</Col>
