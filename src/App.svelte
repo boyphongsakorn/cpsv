98305183d7c0bea78ed969ecd80886034e624455
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-10-24 13:19:19 +0700

    ok

--- src/App.svelte
+++ src/App.svelte
@@ -282,11 +282,11 @@
 									<!-- remove # from item.user -->
 									<Button outline color="primary" on:click={() => wowplayer(item.userid)} style="margin-right: 5px;display: none;"><Avatar randomBgColor initials="{item.user.replace('#', '')}" src="https://minecraftfaces.com/wp-content/bigfaces/big-{item.user.replace('#', '')}-face.png"/></Button>
 									<a href="https://mccplog.pwisetthon.com/?id={item.userid}" id="userid{item.userid}"><Button outline color="primary" style="margin-right: 5px;"><Avatar randomBgColor initials="{item.user.replace('#', '')}" src="https://minecraftfaces.com/wp-content/bigfaces/big-{item.user.replace('#', '')}-face.png"/></Button></a>
-									<Tooltip target="userid{item.userid}" bottom>{item.user.replace('#', '')}</Tooltip>
+									<Tooltip target="userid{item.userid}" placement="bottom">{item.user.replace('#', '')}</Tooltip>
 								{:else}
 									<Button outline color="primary" on:click={() => wowplayer(item.userid)} style="margin-right: 5px;display: none;"><Avatar randomBgColor initials="{item.user}" src="https://cravatar.eu/avatar/{item.user}"/></Button>
 									<a href="https://mccplog.pwisetthon.com/?id={item.userid}" id="userid{item.userid}"><Button outline color="primary" style="margin-right: 5px;"><Avatar randomBgColor initials="{item.user}" src="https://cravatar.eu/avatar/{item.user}"/></Button></a>
-									<Tooltip target="userid{item.userid}" bottom>{item.user}</Tooltip>
+									<Tooltip target="userid{item.userid}" placement="bottom">{item.user}</Tooltip>
 								{/if}
 								
 							{/each}
